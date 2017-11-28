466aeb310709adf64089e37115b858bfa56ad7c4
ovchinnikova <ovchinnikova16@inbox.ru>
2017-11-29 01:19:46 +0300

    хоть как-то работает

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
-using NUnit.Framework;
-using FluentAssertions;
 
 namespace TagsCloudVisualization
 {
